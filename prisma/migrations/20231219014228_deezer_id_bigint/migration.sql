6447e646d9046b0a601f8ed31f57a365c74d0935
Matheus Dias <matheus.sanches@arcotech.io>
2023-12-18 22:42:54 -0300

    chore: change deezer_id int type

---
+++ prisma/migrations/20231219014228_deezer_id_bigint/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "Album" ALTER COLUMN "deezer_id" SET DATA TYPE BIGINT;
+
+-- AlterTable
+ALTER TABLE "Artist" ALTER COLUMN "deezer_id" SET DATA TYPE BIGINT;
+
+-- AlterTable
+ALTER TABLE "Track" ALTER COLUMN "deezer_id" SET DATA TYPE BIGINT;
